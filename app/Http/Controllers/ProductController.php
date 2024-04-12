884874d0ebed2a7b248009740d9f514f7b57015a
durdiev9043 <donyorbek9043@gmail.com>
2024-04-12 22:09:00 +0500

    jkfgf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,7 @@ class ProductController extends Controller
     public function index()
     {
         $cats=Category::whereNull('cat_id')->get();
-        $products=Product::paginate(100);
+        $products=Product::paginate(50);
         return view('admin.product.index',['cats'=>$cats,'products'=>$products]);
     }
 
