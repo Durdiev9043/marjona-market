6e3dc067ee579fb97b3c3e1bfa3b9173d604a206
Bekmurod3388 <bekmurod3388@gmail.com>
2024-07-20 10:55:58 +0300

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
     {
         $cats=Category::whereNull('cat_id')->get();
         $hashs=Category::whereNotNull('cat_id')->get();
-        $products=Product::paginate(10);
+        $products=Product::with('category')->paginate(10);
         return view('admin.product.index',['cats'=>$cats,'products'=>$products,'hashs'=>$hashs]);
     }
 
