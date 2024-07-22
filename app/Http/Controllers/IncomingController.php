7d2dc4a7fb047e33b48985245f6689aa47cba164
durdiev9043 <donyorbek9043@gmail.com>
2024-07-22 16:56:36 +0500

    jkfgf

--- app/Http/Controllers/IncomingController.php
+++ app/Http/Controllers/IncomingController.php
@@ -11,7 +11,7 @@ class IncomingController extends Controller
 
     public function index()
     {
-        $data=IncomingProduct::all();
+        $data=IncomingProduct::with('product')->paginate(10);
         return view('admin.incoming.index',['data'=>$data]);
     }
 
