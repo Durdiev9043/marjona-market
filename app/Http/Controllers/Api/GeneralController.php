ed31c739f9d58a54d043b10217fd5ccf420083d6
durdiev9043 <donyorbek9043@gmail.com>
2024-08-30 22:06:42 +0500

    jkfgf

--- app/Http/Controllers/Api/GeneralController.php
+++ app/Http/Controllers/Api/GeneralController.php
@@ -157,7 +157,7 @@ class GeneralController extends BaseController
        return $this->sendSuccess($tt,'Dokondagi barcha Mahsulotlar');
     }
     public function productfilter($id){
-        $products=Product::where('category_id',$id)->get();
+        $products=Product::where('category_id',$id)->orderBy('hash_id')->get();
 //        $products = Product::where(function($query) use ($id) {
 //            $query->where('category_id', $id)
 //                ->orWhere('count', '>', 0)
