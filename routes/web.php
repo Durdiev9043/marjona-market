416518775f0191af8a243cd812c174e65bae604b
durdiev9043 <donyorbek9043@gmail.com>
2024-09-23 10:28:28 +0500

    jkfgfhjgfhkgf

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::group(['middleware' => ['role:admin']], function () {
     Route::get('clear/card',[\App\Http\Controllers\GeneralController::class,'clearCart'])->name('clearcart');
     Route::get('/less/product',[\App\Http\Controllers\GeneralController::class,'productLess'])->name('product.less');
     Route::get('/minus/product',[\App\Http\Controllers\GeneralController::class,'productMinus'])->name('product.minus');
-    Route::get('/pp/up',[\App\Http\Controllers\GeneralController::class,'ppup']);
+//    Route::get('/pp/up',[\App\Http\Controllers\GeneralController::class,'ppup']);
 //    Route::get('product/less',[\App\Http\Controllers\GeneralController::class,'productLess'])->name('product.less');
     Route::get('/check/{id}', [PdfGeneratorController::class, 'index'])->name('check');
     Route::post('/qr/', [PdfGeneratorController::class, 'gen'])->name('gen');
